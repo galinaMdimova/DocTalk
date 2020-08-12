cce879c39f8bc15c4bc86c69baf78b6a8feff3ac
galinaMdimova <galina_marinova_dimova@abv.bg>
2020-08-12 23:49:19 +0300

    add privateroute

---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Dashboard = (props)=>{
+    return(
+        <div>
+            Меню
+        </div>
+    )
+}
+
+Dashboard.propTypes ={
+
+}
+
+export default Dashboard
+
